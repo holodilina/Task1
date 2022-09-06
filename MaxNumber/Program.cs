d7a4e8373b17c904f4fd636a9854c8c2aaf0e35d
Natalya <holodilina.n@gmail.com>
2022-09-07 00:03:17 +0300

    Add folder MaxNumber

---
+++ MaxNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+{
+    Console.WriteLine("ВВЕДИТЕ ТРИ ЧИСЛА ДЛЯ СРАВНЕНИЯ");
+    int a, b, c;
+    Console.Write("Введите первое число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третье число: ");
+    c = Convert.ToInt32(Console.ReadLine());
+
+    {
+        int max = a;
+
+        if (a > max) max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        Console.Write("Максимальное число: max = ");
+        Console.WriteLine(max);
+    }
+    
+}
